f738943deca722e6342b9110e8d9a27561a2bbb0
Neel Lodha <N.Lodha-1@student.tudelft.nl>
2025-02-14 19:10:22 +0100

    fix homing

--- PI/src/mason_navigation/src/homing_sequence.cpp
+++ PI/src/mason_navigation/src/homing_sequence.cpp
@@ -1,67 +1,49 @@
 #include "mason_navigation/srv/homing_sequence.hpp"
-#include "mason_test/srv/float.hpp"
-#include "std_srvs/srv/empty.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "odrive_can/msg/controller_status.hpp"
 
-#include "rclcpp/rclcpp.hpp"
-#include <memory>
-#include <vector>
 #include <cmath>
+#include <memory>
 #include <string>
+#include <vector>
+
+#include "mason_test/srv/float.hpp"
+#include "odrive_can/msg/controller_status.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_srvs/srv/empty.hpp"
 
 using namespace std::chrono_literals;
-std::vector<std::string> joint_names = {"motor_horizontal", "motor_left", "motor_right"};
 
 class HomingSequenceService : public rclcpp::Node {
-
-public:
+   public:
     HomingSequenceService() : Node("homing_sequence_service") {
-        this->homing_service_ = this->create_service<mason_navigation::srv::HomingSequence>(
-            "home_mason", 
-            std::bind(&HomingSequenceService::handle_homing, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
-        );
+        this->homing_service = this->create_service<mason_navigation::srv::HomingSequence>(
+            "home_mason",
+            std::bind(&HomingSequenceService::handle_homing, this, std::placeholders::_1,
+                      std::placeholders::_2, std::placeholders::_3));
 
-        this->return_to_start_service_ = this->create_service<mason_navigation::srv::HomingSequence>(
-            "return_mason_to_start", 
-            std::bind(&HomingSequenceService::handle_return_to_start, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
-        );
+        this->return_to_start_service_ =
+            this->create_service<mason_navigation::srv::HomingSequence>(
+                "return_mason_to_start",
+                std::bind(&HomingSequenceService::handle_return_to_start, this,
+                          std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
 
-        this->contact_sensor_sub_ = this->create_subscription<std_msgs::msg::String>(
-            "/contact_sensor_triggered", 10,
-            [this](const std_msgs::msg::String::SharedPtr msg) {
+        this->contact_sensor_sub = this->create_subscription<std_msgs::msg::String>(
+            "/contact_sensor_triggered", 10, [this](const std_msgs::msg::String::SharedPtr msg) {
                 this->sensor = msg->data;
                 RCLCPP_INFO(this->get_logger(), "%s CONTACT SENSOR TRIGGERED.", msg->data.c_str());
-            }
-        );
-
-        this->sub0 = this->create_subscription<odrive_can::msg::ControllerStatus>(
-            "odrive_axis0/controller_status", 10,
-            [this](const odrive_can::msg::ControllerStatus::SharedPtr msg) {
-                if (std::isnan(axis0_offset)) axis0_offset = msg->pos_estimate;
-                this->joint_positions[joint_names[0]] = msg->pos_estimate;
-                RCLCPP_INFO(this->get_logger(), "%s position: %f", joint_names[0].c_str(), msg->pos_estimate);
             });
-        this->sub1 = this->create_subscription<odrive_can::msg::ControllerStatus>(
-            "odrive_axis1/controller_status", 10,
-            [this](const odrive_can::msg::ControllerStatus::SharedPtr msg) {
-                if (std::isnan(axis1_offset)) axis1_offset = msg->pos_estimate;
-                this->joint_positions[joint_names[1]] = msg->pos_estimate;
-                RCLCPP_INFO(this->get_logger(), "%s position: %f", joint_names[1].c_str(), msg->pos_estimate);
-            });
-        this->sub2 = this->create_subscription<odrive_can::msg::ControllerStatus>(
-            "odrive_axis2/controller_status", 10,
-            [this](const odrive_can::msg::ControllerStatus::SharedPtr msg) {
-                if (std::isnan(axis2_offset)) axis2_offset = msg->pos_estimate;
-                this->joint_positions[joint_names[2]] = msg->pos_estimate;
 
-                RCLCPP_INFO(this->get_logger(), "%s position: %f", joint_names[2].c_str(), msg->pos_estimate);
+        this->position_sub = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+            "odrive_position", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                this->pos_x = msg->data[0];
+                this->pos_y = msg->data[1];
             });
 
         this->position_y_client_ = this->create_client<mason_test::srv::Float>("odrive_position_y");
-        this->position_x_client_ = this->create_client<mason_test::srv::Float>("odrive_position_x");
+        this->position_x_client = this->create_client<mason_test::srv::Float>("odrive_position_x");
         this->stop_y_client_ = this->create_client<std_srvs::srv::Empty>("odrive_stop_y");
-        this->stop_x_client_ = this->create_client<std_srvs::srv::Empty>("odrive_stop_x");
+        this->stop_x_client = this->create_client<std_srvs::srv::Empty>("odrive_stop_x");
 
         RCLCPP_INFO(this->get_logger(), "Service is ready to receive requests.");
     }
@@ -69,7 +51,8 @@ public:
     bool positionServiceIsAvailable(rclcpp::Client<mason_test::srv::Float>::SharedPtr client) {
         while (!client->wait_for_service(1s)) {
             if (!rclcpp::ok()) {
-                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for position service. Exiting...");
+                RCLCPP_ERROR(this->get_logger(),
+                             "Interrupted while waiting for position service. Exiting...");
                 return false;
             }
             RCLCPP_INFO(this->get_logger(), "Position service not available, waiting again...");
@@ -80,7 +63,8 @@ public:
     bool stoppingServiceIsAvailable(rclcpp::Client<std_srvs::srv::Empty>::SharedPtr client) {
         while (!client->wait_for_service(1s)) {
             if (!rclcpp::ok()) {
-                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for stopping service. Exiting...");
+                RCLCPP_ERROR(this->get_logger(),
+                             "Interrupted while waiting for stopping service. Exiting...");
                 return false;
             }
             RCLCPP_INFO(this->get_logger(), "Stopping service not available, waiting again...");
@@ -88,12 +72,11 @@ public:
         return true;
     }
 
-private:
+   private:
     void handle_homing(
-      const std::shared_ptr<rmw_request_id_t> /*request_header*/,
-      const std::shared_ptr<mason_navigation::srv::HomingSequence::Request> /*request*/,
-      const std::shared_ptr<mason_navigation::srv::HomingSequence::Response> response) {
-
+        const std::shared_ptr<rmw_request_id_t> /*request_header*/,
+        const std::shared_ptr<mason_navigation::srv::HomingSequence::Request> /*request*/,
+        const std::shared_ptr<mason_navigation::srv::HomingSequence::Response> response) {
         bool homed_horizontally = false;
         bool homed_vertically = false;
 
@@ -102,7 +85,7 @@ private:
         position_request->input_pos = 1000.0f;
 
         bool ready = false;
-        while(!ready) {
+        while (!ready) {
             ready = this->positionServiceIsAvailable(this->position_y_client_);
             ready = ready && this->stoppingServiceIsAvailable(this->stop_y_client_);
         }
@@ -116,58 +99,42 @@ private:
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(200ms);
 
-        if (joint_positions.find("motor_left") == joint_positions.end() || joint_positions.find("motor_right") == joint_positions.end()) {
-            RCLCPP_ERROR(this->get_logger(),
-                "Error occurred during homing: Joint \"motor_left\" and/or \"motor_right\" not found.");
-        }
-
-        if (std::abs((this->joint_positions["motor_left"] - this->joint_positions["motor_right"])) > 1.0f) {
-            RCLCPP_ERROR(this->get_logger(),
-                "Error occurred during homing: Left and right vertical motors seem misaligned.");
-        }
-
-        response->y_limit = std::min(this->joint_positions["motor_left"], this->joint_positions["motor_right"]);
+        response->y_limit = this->pos_y;
 
         ready = false;
-        while(!ready) {
-            ready = this->positionServiceIsAvailable(this->position_x_client_);
-            ready = ready && this->stoppingServiceIsAvailable(this->stop_x_client_);
+        while (!ready) {
+            ready = this->positionServiceIsAvailable(this->position_x_client);
+            ready = ready && this->stoppingServiceIsAvailable(this->stop_x_client);
         }
 
-        this->position_x_client_->async_send_request(position_request);
+        this->position_x_client->async_send_request(position_request);
 
         while (!homed_horizontally) {
             if (this->sensor == "RIGHT") {
-                this->stop_x_client_->async_send_request(stop_request);
+                this->stop_x_client->async_send_request(stop_request);
                 homed_horizontally = true;
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(200ms);
 
-        if (joint_positions.find("motor_horizontal") == joint_positions.end()) {
-            RCLCPP_ERROR(this->get_logger(),
-                "Error occurred during homing: Joint \"motor_horizontal\" not found.");
-        }
-
-        response->x_limit = this->joint_positions["motor_horizontal"];
+        response->x_limit = this->pos_x;
     }
 
     // Can change to a std_srvs/srv/Trigger type of Service
     // If getting the values of min_x and min_y proves unnecessary
     void handle_return_to_start(
-      const std::shared_ptr<rmw_request_id_t> /*request_header*/,
-      const std::shared_ptr<mason_navigation::srv::HomingSequence::Request> /*request*/,
-      const std::shared_ptr<mason_navigation::srv::HomingSequence::Response> response) {
-
+        const std::shared_ptr<rmw_request_id_t> /*request_header*/,
+        const std::shared_ptr<mason_navigation::srv::HomingSequence::Request> /*request*/,
+        const std::shared_ptr<mason_navigation::srv::HomingSequence::Response> response) {
         auto position_request = std::make_shared<mason_test::srv::Float::Request>();
         auto stop_request = std::make_shared<std_srvs::srv::Empty::Request>();
         position_request->input_pos = -1000.0f;
 
         bool ready = false;
-        while(!ready) {
+        while (!ready) {
             ready = this->positionServiceIsAvailable(this->position_y_client_);
             ready = ready && this->stoppingServiceIsAvailable(this->stop_y_client_);
         }
@@ -181,61 +148,46 @@ private:
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(200ms);
 
-        if (joint_positions.find("motor_left") == joint_positions.end() || joint_positions.find("motor_right") == joint_positions.end()) {
-            RCLCPP_ERROR(this->get_logger(),
-                "Error occurred during homing: Joint \"motor_left\" and/or \"motor_right\" not found.");
-        }
-
-        if (std::abs((this->joint_positions["motor_left"] - this->joint_positions["motor_right"])) > 1.0f) {
-            RCLCPP_ERROR(this->get_logger(),
-                "Error occurred during homing: Left and right vertical motors seem misaligned.");
-        }
-
-        response->y_limit = std::min(this->joint_positions["motor_left"], this->joint_positions["motor_right"]);
+        response->y_limit = this->pos_y;
 
         ready = false;
-        while(!ready) {
-            ready = this->positionServiceIsAvailable(this->position_x_client_);
-            ready = ready && this->stoppingServiceIsAvailable(this->stop_x_client_);
+        while (!ready) {
+            ready = this->positionServiceIsAvailable(this->position_x_client);
+            ready = ready && this->stoppingServiceIsAvailable(this->stop_x_client);
         }
 
-        this->position_x_client_->async_send_request(position_request);
+        this->position_x_client->async_send_request(position_request);
 
         while (true) {
             if (this->sensor == "LEFT") {
-                this->stop_x_client_->async_send_request(stop_request);
+                this->stop_x_client->async_send_request(stop_request);
                 break;
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(200ms);
 
-        if (joint_positions.find("motor_horizontal") == joint_positions.end()) {
-            RCLCPP_ERROR(this->get_logger(),
-                "Error occurred during homing: Joint \"motor_horizontal\" not found.");
-        }
-
-        response->x_limit = this->joint_positions["motor_horizontal"];
+        response->x_limit = this->pos_x;
     }
 
-    rclcpp::Service<mason_navigation::srv::HomingSequence>::SharedPtr homing_service_;
+    rclcpp::Service<mason_navigation::srv::HomingSequence>::SharedPtr homing_service;
     rclcpp::Service<mason_navigation::srv::HomingSequence>::SharedPtr return_to_start_service_;
-    rclcpp::Client<mason_test::srv::Float>::SharedPtr position_x_client_;
+    rclcpp::Client<mason_test::srv::Float>::SharedPtr position_x_client;
     rclcpp::Client<mason_test::srv::Float>::SharedPtr position_y_client_;
-    rclcpp::Client<std_srvs::srv::Empty>::SharedPtr stop_x_client_;
+    rclcpp::Client<std_srvs::srv::Empty>::SharedPtr stop_x_client;
     rclcpp::Client<std_srvs::srv::Empty>::SharedPtr stop_y_client_;
-    rclcpp::Subscription<odrive_can::msg::ControllerStatus>::SharedPtr sub0;
-    rclcpp::Subscription<odrive_can::msg::ControllerStatus>::SharedPtr sub2;
-    rclcpp::Subscription<odrive_can::msg::ControllerStatus>::SharedPtr sub1;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr contact_sensor_sub_;
-    std::unordered_map<std::string, float> joint_positions;
-    float axis0_offset = nanf(""), axis1_offset = nanf(""), axis2_offset = nanf("");
+
+    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr position_sub;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr contact_sensor_sub;
+
+    float pos_x = 0, pos_y;
+
     std::string sensor = "";
 };
 
-int main(int argc, char ** argv) {
+int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
 
     std::shared_ptr<HomingSequenceService> service_node = std::make_shared<HomingSequenceService>();
@@ -246,4 +198,4 @@ int main(int argc, char ** argv) {
     rclcpp::shutdown();
 
     return 0;
-} 
+}
